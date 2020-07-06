5cccaf5f96284dea15f98fe5eae9654a02d95f1d
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-07-05 21:27:23 -0300

    Factory for http response

---
+++ src/presentation/helpers/http-response.js
@@ -0,0 +1,38 @@
+const { UnauthorizedError, ServerError } = require('../errors') // Importa por default o index.js da pasta errors
+
+module.exports = class HttpResponse {
+  static badRequest (error) {
+    return {
+      statusCode: 400,
+      body: error
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500,
+      body: new ServerError()
+    }
+  }
+
+  static NotFoundError (error) {
+    return {
+      statusCode: 404,
+      body: error
+    }
+  }
+
+  static unauthorizedError () {
+    return {
+      statusCode: 401,
+      body: new UnauthorizedError()
+    }
+  }
+
+  static ok (data) {
+    return {
+      statusCode: 200,
+      body: data
+    }
+  }
+}
